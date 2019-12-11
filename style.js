74735b18aa21e588f93bdacb46cbd4585d7d04d5
Andres Sanchez <45046540+andresssanchez@users.noreply.github.com>
2019-12-11 09:07:10 -0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+function changePicture(elem){
+	document.getElementById("NJ").src = "images/NHC.jpg";
+}
+function changeImage(elem){
+	document.getElementById("Shoes").src = "images/REDJ.jpg";
+}
+function changePhoto(elem){
+	document.getElementById("Jersey").src = "images/giantsJ.png";
+}
+function playSound (elem) {
+	document.getElementById("hot-sound").play();
+}
